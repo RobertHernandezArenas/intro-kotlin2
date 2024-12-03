e168eea9f938f7496c5e9a2314c76c072165b4e7
Robert Hernández <roberthernandezarenas@gmail.com>
2024-12-03 22:05:32 +0100

    :hammer: Added replace char feature

--- src/Main.kt
+++ src/Main.kt
@@ -5,9 +5,7 @@ import integers.myInteger
 import longs.myLong
 import stringexercises.StringExercises
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val exercise = StringExercises()
-    exercise.create()
+    exercise.start()
 }
--- src/stringexercises/StringExercises.kt
+++ src/stringexercises/StringExercises.kt
@@ -5,35 +5,92 @@ import java.util.*
 class StringExercises {
 
     // Method for creating and launching an exercise
-    fun create() {
+    fun start() {
+
+        // Get user input from prompt terminal
         val scanner = Scanner(System.`in`)
 
         println("Select a string exercise (1-6):")
-        println("1. String concat")
+        println("1. Concat 2 strings")
+        println("2. Replace char in string")
 
         val input = scanner.nextInt()
 
         when (input) {
-           1 -> {
+
+            // Exercise 1
+            1 -> {
                 println("You have selected option number 1")
 
                 // Get first string
                 println("Please, Insert the first string:")
-                val firstString : String = readlnOrNull().toString()
+                val firstString: String = readlnOrNull().toString()
 
                 // Get second string
                 println("Please, Insert the second string:")
-                val secondString : String = readlnOrNull().toString()
+                val secondString: String = readlnOrNull().toString()
 
                 // Concat string result
                 println("[String concat Output] ======> ${concatStrings(firstString, secondString)}")
             }
 
+            // Exercise 2
+            2 -> {
+                println("You have selected option number 2")
+
+                // Get text
+                println("Please, Insert text:")
+                val phrase: String = readlnOrNull().toString()
+
+                // Get char to search in previous text
+                println("Please, Insert character to search:")
+                val charToSearch: String = readlnOrNull().toString()
+
+                // Convert charToSearch: String to Char
+                val hasChar = phrase.contains(charToSearch.single())
+
+                // Check if charToSearch is in phrase
+                if (!hasChar) {
+                    println("The character you have selected does not exist in the phrase")
+
+                    // TODO: Repeat action to insert char to search
+
+                    // Throw exception error if charToSearch is not in phrase
+                    throw Exception("The character you have selected does not exist in the phrase")
+                }
+
+                // Get char to replace
+                println("Please, Insert a character to replace:")
+
+                // Convert charToReplace: String to Char
+                val charToReplace = scanner.next().single()
+
+                // Replace char result
+                println(
+                    "[Char replace Output] ======> ${
+                        replaceChar(
+                            phrase,
+                            charToSearch.single(),
+                            charToReplace
+                        )
+                    }"
+                )
+            }
+
+            // Default if user does not select an exercise or the exercise does not exist
             else -> println("The exercise you have selected does not exist")
         }
     }
 
+    // Methods class
+
+    // Exercise 1
     private fun concatStrings(input1: String, input2: String): String {
         return "$input1 $input2"
+    }
+
+    // Exercise 2
+    private fun replaceChar(phrase: String, charToSearch: Char, charToReplace: Char): String {
+        return phrase.replace(Regex(charToSearch.toString()), charToReplace.toString())
     }
 }
