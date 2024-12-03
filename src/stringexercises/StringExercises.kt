db937ad4c2994a942d71291ce11700be22124c7b
Robert Hernández <roberthernandezarenas@gmail.com>
2024-12-03 21:32:10 +0100

    :recycle: Refactorized concat string exercise

--- src/stringexercises/StringExercises.kt
+++ src/stringexercises/StringExercises.kt
@@ -14,15 +14,20 @@ class StringExercises {
         val input = scanner.nextInt()
 
         when (input) {
-            1 -> {
+           1 -> {
                 println("You have selected option number 1")
-                println("Please, Insert two strings to concat:")
-                val (input1, input2) = getTwoStrings()
-                println(input1)
-                println(input2)
-                println("Result: ${concatStrings(input1, input2)}")
-            }
 
+                // Get first string
+                println("Please, Insert the first string:")
+                val firstString : String = readlnOrNull().toString()
+
+                // Get second string
+                println("Please, Insert the second string:")
+                val secondString : String = readlnOrNull().toString()
+
+                // Concat string result
+                println("[String concat Output] ======> ${concatStrings(firstString, secondString)}")
+            }
 
             else -> println("The exercise you have selected does not exist")
         }
@@ -31,12 +36,4 @@ class StringExercises {
     private fun concatStrings(input1: String, input2: String): String {
         return "$input1 $input2"
     }
-
-    // Methods class
-    private fun getTwoStrings(): Pair<String, String> {
-        val a = readlnOrNull().toString()
-        val b = readlnOrNull().toString()
-        return Pair(a, b)
-    }
-
 }
