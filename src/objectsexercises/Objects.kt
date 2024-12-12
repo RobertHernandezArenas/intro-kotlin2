5c4230cf9078dc041710fdcf76a98195d6a4595c
Robert Hernández <roberthernandezarenas@gmail.com>
2024-12-13 00:19:20 +0100

    :wrench: Added feature/CreateCar

--- src/objectsexercises/Objects.kt
+++ src/objectsexercises/Objects.kt
@@ -3,11 +3,17 @@ package objectsexercises
 // Exercise 1
 data class Brand(val name: String, val year: Int, val country: String)
 
+// Exercise 2
+data class Car(val model: String, val year: Int, val hp: Int, val brand: String)
+
 class ObjectsExercise {
 
     // Exercise 1
     fun createBrand(name: String, year: Int, country: String): Brand = Brand(name, year, country)
 
+
+    // Exercise 2
+    fun createCar(model: String, year: Int, hp: Int, brand: String): Car = Car(model, year, hp, brand)
 
 }
 
@@ -19,4 +25,11 @@ fun main() {
     println(newBrand)
 
 
+    /*
+    Ejercicio 2
+Crea una función con los parámetros que necesites que cree y devuelva un objeto coche que tenga un nombre de modelo, fecha de fabricación, número de caballos y marca
+    */
+
+    val newCar = ObjectsExercise().createCar("BMW", 1897, 500, "Alemania").toString()
+    println(newCar)
 }
